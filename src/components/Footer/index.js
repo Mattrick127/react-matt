3992bd239cecffb9a17f7b5780ad98db9d6a6776
matt <mattrick2214@gmail.com>
2022-04-03 01:31:56 -0500

    Added more information at footer, including links to my three sites.

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,9 +3,14 @@ import React from "react";
 function Footer() {
     return (
         <footer id="footer">
-            YO YO YO!
+            For more information on me, please visit these links:
+            <a href="https://github.com/Mattrick127">Github, </a>
+            <a href="https://www.linkedin.com/in/matthew-wilson-5b8421179/">LinkedIn, </a> and
+            <a href="https://stackoverflow.com/users/16998870/matthew-wilson"> Stack Overflow</a>
         </footer>
     )
 }
-
+// https://github.com/Mattrick127
+// https://www.linkedin.com/in/matthew-wilson-5b8421179/
+// https://stackoverflow.com/users/16998870/matthew-wilson
 export default Footer;
