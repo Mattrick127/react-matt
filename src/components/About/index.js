23d00ac41c9a87e830fd91fd300ca5c15ebd1d7a
matt <mattrick2214@gmail.com>
2022-04-03 01:49:44 -0500

    A little more in a short bio and a pic of me!

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,14 +1,16 @@
 import React from "react";
+import love from "../../assets/images/love.jpg";
 
 function About() {
     return (
         <section className="my-5">
-            <h1 id="about">Hello, my name is Matthew Wilson.</h1>
+            <h1 id="about">My name is Matt, and welcome to my first react.js profile page!</h1>
             <div className="my-2">
-                <p>
-                I am currently a student going through UTSA coding boot camp.
-                </p>
+                <p>I am currently a UTSA coding boot camp student. I'm almost done, and this has been probably the most challenging thing I've ever done in my life.</p>
+                <p>I've learned alot though, and I plan to study more outside of this class so I can feel confident in this new career field.</p>
+                <p>I (of course) like video games and making sure my girlfriend and two dogs are happy and healthy.</p>
             </div>
+            <img src={love} alt="love"></img>
         </section>
     )
 }
